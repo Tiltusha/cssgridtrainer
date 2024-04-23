78e88c77d66e797a9551675e05f701241d8bdeac
Tiltusha <elcoronabooking@gmail.com>
2024-04-23 22:24:07 +0300

    навел красоту

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -56,26 +56,37 @@
 
 </script>
 
-<main>
-  <div class="container">
-    <div class="title">
-      <h1 class="title__h1">Конвертер валют</h1>
+<main class="app">
+  <div class="app__container">
+    <div class="app__header">
+      <div class="app__logo-container">
+        <img src="https://avatars.githubusercontent.com/u/128232731?s=400&u=f71cd561e85e796d249cd73fbd36be3bc4501d71&v=4" alt="Logo" class="app__logo">
+      </div>
+      <h1 class="app__title">Конвертер валют</h1>
     </div>
-    <div class="currency">
-      <input type="number" bind:value={amountFrom} on:input={(e) => handleAmountChange(e, 'from')} />
-      <select bind:value={currencyFrom} on:change={(e) => handleCurrencyChange(e, 'from')}>
-        {#each currencies as currency}
-          <option value={currency}>{currency}</option>
-        {/each}
-      </select>
+    <div class="app__form">
+      <div class="form__row">
+        <input type="number" bind:value={amountFrom} on:input={(e) => handleAmountChange(e, 'from')} class="form__input form__input--from"/>
+        <select bind:value={currencyFrom} on:change={(e) => handleCurrencyChange(e, 'from')} class="form__select">
+          {#each currencies as currency}
+            <option value={currency}>{currency}</option>
+          {/each}
+        </select>
+      </div>
+      <div class="form__row">
+        <input type="number" bind:value={amountTo} on:input={(e) => handleAmountChange(e, 'to')} class="form__input form__input--to"/>
+        <select bind:value={currencyTo} on:change={(e) => handleCurrencyChange(e, 'to')} class="form__select">
+          {#each currencies as currency}
+            <option value={currency}>{currency}</option>
+          {/each}
+        </select>
+      </div>
     </div>
-    <div class="currency">
-      <input type="number" bind:value={amountTo} on:input={(e) => handleAmountChange(e, 'to')} />
-      <select bind:value={currencyTo} on:change={(e) => handleCurrencyChange(e, 'to')}>
-        {#each currencies as currency}
-          <option value={currency}>{currency}</option>
-        {/each}
-      </select>
+  </div>
+  <div class="app__footer">
+    <div class="footer__signature">
+      <p class="footer__text">Создано с помощью</p>
+      <p class="footer__author">Tiltusha</p>
     </div>
   </div>
 </main>
@@ -86,64 +97,149 @@
     margin: 0;
     padding: 0;
   }
-  main {
+  .app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+    background-image: linear-gradient(to bottom right, #7f7fd5, #86a8d8, #8493c7, #5c6bc0, #8e24aa, #ab47bc);
+    animation: fadeIn 1s ease-in-out forwards;
+  }
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+      transform: scale(0.95);
+    }
+    100% {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+  .app__container {
+    margin: 0 auto;
+    max-width: 400px;
+    flex: 1;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    height: 100vh;
-    width: 100vw;
-    margin: 0;
     padding: 20px;
-    background-color: pink;
   }
-  .title__h1 {
-    padding-bottom: 1.5rem;
+  .app__header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 20px;
+    animation: fadeIn 1.5s ease-in-out forwards;
   }
-  .container {
-    background-color: #fff;
-    padding: 8px 40px;
-    border-radius: 10px;
-    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
+  .app__logo-container {
+    animation: pulse 1.5s infinite;
+    padding-right: 10px;
   }
-  .btn {
-    color: #fff;
-    background-color: pink;
-    cursor: pointer;
+  @keyframes pulse {
+    0% {
+      transform: scale(0.95);
+      opacity: 0.7;
+    }
+    100% {
+      transform: scale(1);
+      opacity: 1;
+    }
+  }
+  .app__logo {
+    height: 50px;
+    animation: fadeIn 1.5s ease-in-out forwards;
+  }
+  .app__title {
+    font-size: 24px;
+    font-weight: 600;
+    animation: printText 2s ease-in forwards;
+  }
+  @keyframes printText {
+    from {
+      color: transparent;
+    }
+    to {
+      color: #fff;
+    }
+  }
+  .app__form {
+    display: flex;
+    flex-direction: column;
+    animation: fadeIn 1.5s ease-in-out forwards;
+  }
+  .form__row {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+    width: 100%;
+    animation: fadeIn 2s ease-in-out forwards;
+  }
+  .form__input {
+    padding: 10px;
+    border: none;
     border-radius: 5px;
-    font-size: 15px;
-    padding: 5px 15px;
+    font-size: 24px;
+    background-color: #fff;
+    color: #333;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
+    animation: animateInput 1s ease-in-out forwards;
+    outline: none;
   }
-  .currency {
-    padding: 40px 0;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  @keyframes animateInput {
+    0% {
+      transform: scale(0.9);
+      opacity: 0.7;
+    }
+    100% {
+      transform: scale(1);
+      opacity: 1;
+    }
   }
-  .currency select {
-    padding: 10px 20px 10px 10px;
+  .form__select {
+    padding: 10px;
+    background-color: #fff;
+    border: none;
+    border-radius: 5px;
     appearance: none;
-    border: 1px solid #dedede;
-    font-size: 16px;
-    background: transparent;
+    font-size: 20px;
+    font-weight: 600;
+    color: #484848;
+    animation: fadeIn 2s ease-in-out forwards;
+    outline: none;
+    
   }
-  .currency input {
-    border: 0;
-    background: transparent;
-    font-size: 30px;
-    text-align: left;
+  .form__input--from {
+    animation-delay: 0.2s;
   }
-  .swap-rate-container {
+  .form__input--to {
+    animation-delay: 0.4s;
+  }
+  .app__footer {
     display: flex;
+    justify-content: center;
+    animation: fadeIn 2s ease-in-out forwards;
+  }
+  .footer__signature {
+    display: flex;
+    flex-direction: column;
     align-items: center;
-    justify-content: space-between;
+    padding: 20px;
   }
-  .rate {
-    color: #9e9e9e;
-    font-size: 14px;
-    padding: 0 10px;
+  .footer__text {
+    font-size: 12px;
+    color: #aaa;
+    margin-bottom: 10px;
+    animation: fadeIn 2s ease-in-out forwards;
   }
-  select:focus, input:focus, button:focus {
-    outline: 0;
+  .footer__author {
+    font-size: 16px;
+    font-weight: 600;
+    color: #fff;
+    animation: fadeIn 2s ease-in-out forwards;
   }
 </style>
+
+
+
+
